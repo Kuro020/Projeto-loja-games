dbb803663e23a83506eefaaa6a3c258864b6e37c
Mateus <runescapeshiro02@gmail.com>
2021-11-23 17:23:28 -0300

    agony

--- view/cadastroPedido.php
+++ view/cadastroPedido.php
@@ -3,46 +3,68 @@ include_once("header.php");
 include_once("../model/bancoPedido.php");
 include_once("../model/conexao.php");
 include_once("../model/bancoFuncionario.php");
+include_once("../model/bancoCliente.php");
+include_once("../model/bancoJogos.php");
 
 $codUsuFK = $_SESSION["codigoUsuario"];
-$funcionario = listaBuscaFuncionarioUsuario($conexao,$codUsuFK);
+$funcionario = listaBuscaFuncionarioUsuario($conexao, $codUsuFK);
 ?>
-<form class="row g-3">
+<div class="row g-3">
     <div class="col-md-3">
         <label for="inputCodFun" class="form-label">Código</label>
-        <input type="text" value="<?php echo($funcionario["codFun"])?>" class="form-control" id="inputCodFun">
+        <input type="text" value="<?php echo ($funcionario["codFun"]) ?>" class="form-control" id="inputCodFun">
     </div>
     <div class="col-md-9">
         <label for="inputNomeFun" class="form-label">Funcionário</label>
-        <input type="text" value="<?php echo($funcionario["nomeFun"])?>" class="form-control" id="inputNomeFun">
+        <input type="text" value="<?php echo ($funcionario["nomeFun"]) ?>" class="form-control" id="inputNomeFun">
     </div>
-    <div class="col-md-2">
-        <label for="inputCodCli" class="form-label">Código</label>
-        <input type="text" class="form-control" id="inputCodCli">
+    <?php
+    $codCliente = isset($_POST["codCliente"]) ? $_POST["codCliente"] : 0;
+    $clientes = isset($codCliente) ? listaTudoClientesCod($conexao, $codCliente) : "";
+    $_SESSION["codigoCliente"] = isset($_POST["codCliente"]) ? $_POST["codCliente"] : "0";
+    $_SESSION["nomeCliente"] = isset($clientes["nomeCli"]) ? $clientes["nomeCli"] : "";
+    ?>
+    <div class="col-md-3">
+        <label for="inputCodCli" required class="form-label">Código</label>
+        <form method="post" action="cadastroPedido.php">
+            <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                <input type="text" class="form-control" value="<?= $_SESSION["codigoCliente"] ?>" id="inputCodCli" required name="codCliente">
+                <button class="btn btn-success me-md-2" type="submit">Pesquisar</button>
+            </div>
+        </form>
     </div>
-    <div class="col-md-1">
-        <label for="inputCodCli" class="form-label">Pesquisar</label>
-        <button type="button" class="btn btn-success">Busca</button>
-    </div>
+    <?php
+ 
+    ?>
     <div class="col-md-9">
         <label for="inputNomeCli" class="form-label">Cliente</label>
-        <input type="text" class="form-control" id="inputNomeCli">
+        <input type="text" value="<?= $_SESSION["nomeCliente"] ?>" class="form-control" id="inputNomeCli">
     </div>
-    <div class="col-md-2">
+    <?php
+    $codJogo = isset($_POST["codJogo"]) ? $_POST["codJogo"] : "0";
+    ?>
+    <div class="col-md-3">
         <label for="inputCodJog" class="form-label">Código</label>
-        <input type="text" class="form-control" id="inputCodJog">
+        <form method="post" action="cadastroPedido.php">
+            <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                <input type="text" class="form-control" value="<?= $codJogo ?>" id="inputCodJog" required name="codJogo">
+                <input type="hidden" value="<?=$_SESSION['codigoCliente']?>" name="codCliente">
+                <input type="hidden" value="<?=$_SESSION['nomeCliente']?>" name="nomeCliente">
+                <button class="btn btn-success me-md-2" type="submit">Pesquisar</button>
+            </div>
+        </form>
     </div>
-    <div class="col-md-1">
-        <label for="inputCodCli" class="form-label">Pesquisar</label>
-        <button type="button" class="btn btn-success">Busca</button>
-    </div>
+    <?php
+    $jogos = isset($codJogo) ? listaTudoJogosCod($conexao, $codJogo) : "";
+    $_SESSION["codigoJogo"] = isset($_POST["codJogo"]) ? $_POST["codJogo"] : "0";
+    ?>
     <div class="col-md-4">
         <label for="inputNomeJog" class="form-label">Jogo</label>
-        <input type="text" class="form-control" id="inputNomeJog">
+        <input type="text" value="<?= $jogos['nomeJog'] ?>" class="form-control" id="inputNomeJog">
     </div>
     <div class="col-md-1">
         <label for="inputQtdJog" class="form-label">Quantidade</label>
-        <select id="inputQtdJog" class="form-select">
+        <select id="inputQtdJog" class="form-select" onchange="Quantidadejogo(this.value)">
             <option selected>Escolha...</option>
             <option value=1>1</option>
             <option value=2>2</option>
@@ -53,7 +75,7 @@ $funcionario = listaBuscaFuncionarioUsuario($conexao,$codUsuFK);
     </div>
     <div class="col-md-2">
         <label for="inputUnitario" class="form-label">Valor Unitário</label>
-        <input type="text" class="form-control" id="inputUnitario">
+        <input type="text" value="<?= $jogos['precoJog'] ?>" class="form-control" id="inputUnitario">
     </div>
     <div class="col-md-2">
         <label for="inputTotal" class="form-label">Valor Total</label>
@@ -62,7 +84,13 @@ $funcionario = listaBuscaFuncionarioUsuario($conexao,$codUsuFK);
     <div class="col-12">
         <button type="submit" class="btn btn-success">Confirmar</button>
     </div>
-</form>
+</div>
+
+<script>
+    function Quantidadejogo(inputQtdJog){
+        alert(doSomething(inputQtdJog));
+    }
+</script>
 <?php
 include_once("footer.php");
 ?>
